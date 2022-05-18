c20daedad63476131e3272143c2f0e620facc9f8
Younes Askour <yaskour@e1r5p1.1337.ma>
2022-05-18 14:11:42 +0100

    start working on arguments

--- philo.c
+++ philo.c
@@ -6,38 +6,54 @@
 /*   By: yaskour <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/03 15:00:31 by yaskour           #+#    #+#             */
-/*   Updated: 2022/05/17 14:32:41 by yaskour          ###   ########.fr       */
+/*   Updated: 2022/05/18 14:09:56 by yaskour          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "philo.h"
 #include <pthread.h>
 #include <unistd.h>
+#include <signal.h>
+#include "libft/libft.h"
 
-void *printNUMber(void *t)
+//void	*printnumber(void *t)
+//{
+//  printf("test 1 \n");
+//  sleep(3);
+//  printf("----------\n");
+//  return (t);
+//}
+
+int	main(int ac, char **av)
 {
-  printf("test 1 \n");
-  sleep(3);
-  printf("----------\n");
-  return (t);
+	philo philos;
+	//int				t;
+	//pthread_t		id;
+
+	if (ac == 5 || ac == 6)
+	{
+		philos.number_of_philo = ft_atoi(av[1]);
+		philos.time_to_die = ft_atoi(av[2]);
+		philos.time_to_eat = ft_atoi(av[3]);
+		philos.time_to_sleep = ft_atoi(av[4]);
+	}
+	if (ac == 6)
+		philos.number_of_times_e_philo_must_eat = ft_atoi(av[5]);
+	if (ac < 5 || ac > 6)
+	{
+		printf("invalid arguments\n");
+		return (0);
+	}
+	printf("num :%d\n",philos.number_of_philo);
+	printf("num :%d\n",philos.time_to_die);
+	printf("num :%d\n",philos.time_to_eat);
+	printf("num :%d\n",philos.time_to_sleep);
+	printf("num :%d\n",philos.number_of_times_e_philo_must_eat);
 }
 
-int main(int ac,char **av)
-{
-  //if (ac >= 5)
-  //{
-  //  printf("number_of_philosophers                    : %s\n",av[1]);
-  //  printf("time_to_die                               : %s\n",av[2]);
-  //  printf("time_to_eat                               : %s\n",av[3]);
-  //  printf("time_to_sleep                             : %s\n",av[4]);
-  //}
-  //if (ac == 6)
-  //  printf("number_of_times_each_philosopher_must_eat : %s\n",av[5]);
-  pthread_t id[2];
-  int a = 1;
-  pthread_create(&id[0],NULL,printNUMber,&a);
-  pthread_create(&id[1],NULL,printNUMber,&a);
-  
-  int *ptr;
-  pthread_join(id[0],(void **)&ptr);
-  pthread_join(id[1],(void **)&ptr);
-} 
+//int a;
+//int *ptr;
+//  a = 1;
+//  pthread_create(&id[0], NULL, printnumber, &a);
+//  pthread_create(&id[1], NULL, printnumber, &a);
+//  pthread_join(id[0], (void **)&ptr);
+//  pthread_join(id[1], (void **)&ptr);
--- philo.h
+++ philo.h
@@ -6,11 +6,19 @@
 /*   By: yaskour <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/03 15:06:19 by yaskour           #+#    #+#             */
-/*   Updated: 2022/04/03 15:07:51 by yaskour          ###   ########.fr       */
+/*   Updated: 2022/05/18 13:50:49 by yaskour          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
 # include <stdio.h>
+typedef struct philo
+{
+  int number_of_philo;
+  int time_to_die;
+  int time_to_eat;
+  int time_to_sleep;
+  int number_of_times_e_philo_must_eat;
+} philo;
 #endif
