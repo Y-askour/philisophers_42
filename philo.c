56bf71d7cf34aa36eee7888944a30163708c9b89
Younes Askour <yaskour@e1r5p1.1337.ma>
2022-05-20 18:18:51 +0100

    start working on project

--- philo.c
+++ philo.c
@@ -6,54 +6,70 @@
 /*   By: yaskour <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/03 15:00:31 by yaskour           #+#    #+#             */
-/*   Updated: 2022/05/18 14:09:56 by yaskour          ###   ########.fr       */
+/*   Updated: 2022/05/20 18:17:55 by yaskour          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "philo.h"
-#include <pthread.h>
-#include <unistd.h>
-#include <signal.h>
-#include "libft/libft.h"
+void	*routine()
+{
+	printf("hey\n");
+	return (NULL);
+}
 
-//void	*printnumber(void *t)
-//{
-//  printf("test 1 \n");
-//  sleep(3);
-//  printf("----------\n");
-//  return (t);
-//}
+void	init_forks(t_info *data)
+{
+	int i = 0;
+	
+	data->forks = malloc(sizeof(pthread_mutex_t) * data->number_of_philo);
+	while(i < data->number_of_philo)
+	{
+		pthread_mutex_init(&data->forks[i],NULL);
+		i++;
+	}
+}
+
+void	init_philo(t_info *data,t_philo *philos)
+{
+	int i;
+
+	i = 0;
+	while(i < data->number_of_philo)
+	{
+		philos[i].id = i + 1;
+		pthread_create(&philos->my_thread,NULL,&routine,NULL);
+		philos[i].right_fork = data->forks[i];
+		if (i == 0)
+			philos->leftfork = &data->forks[data->number_of_philo - 1];
+		else
+			philos->leftfork = &data->forks[i - 1];
+		philos->info = data;
+		i++;
+	}
+}
 
 int	main(int ac, char **av)
 {
-	philo philos;
-	//int				t;
-	//pthread_t		id;
+	t_info	data;
+	t_philo *philos;
+	
 
 	if (ac == 5 || ac == 6)
 	{
-		philos.number_of_philo = ft_atoi(av[1]);
-		philos.time_to_die = ft_atoi(av[2]);
-		philos.time_to_eat = ft_atoi(av[3]);
-		philos.time_to_sleep = ft_atoi(av[4]);
+		data.number_of_philo = ft_atoi(av[1]);
+		data.time_to_die = ft_atoi(av[2]);
+		data.time_to_eat = ft_atoi(av[3]);
+		data.time_to_sleep = ft_atoi(av[4]);
 	}
 	if (ac == 6)
-		philos.number_of_times_e_philo_must_eat = ft_atoi(av[5]);
+		data.number_of_times_e_philo_must_eat = ft_atoi(av[5]);
 	if (ac < 5 || ac > 6)
 	{
-		printf("invalid arguments\n");
+		write(1,"invalid arguments\n",18);
 		return (0);
 	}
-	printf("num :%d\n",philos.number_of_philo);
-	printf("num :%d\n",philos.time_to_die);
-	printf("num :%d\n",philos.time_to_eat);
-	printf("num :%d\n",philos.time_to_sleep);
-	printf("num :%d\n",philos.number_of_times_e_philo_must_eat);
+	philos = malloc(sizeof(t_philo) * data.number_of_philo);
+	init_forks(&data);
+	init_philo(&data,philos);
+	sleep(30);
 }
 
-//int a;
-//int *ptr;
-//  a = 1;
-//  pthread_create(&id[0], NULL, printnumber, &a);
-//  pthread_create(&id[1], NULL, printnumber, &a);
-//  pthread_join(id[0], (void **)&ptr);
-//  pthread_join(id[1], (void **)&ptr);
--- philo.h
+++ philo.h
@@ -6,19 +6,39 @@
 /*   By: yaskour <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/03 15:06:19 by yaskour           #+#    #+#             */
-/*   Updated: 2022/05/18 13:50:49 by yaskour          ###   ########.fr       */
+/*   Updated: 2022/05/20 18:05:39 by yaskour          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
+# include <pthread.h>
+#include "libft/libft.h"
+# include <unistd.h>
 # include <stdio.h>
+
+typedef struct args
+{
+  int           number_of_philo;
+  int           time_to_die;
+  int           time_to_eat;
+  int           time_to_sleep;
+  int           number_of_times_e_philo_must_eat;
+  pthread_mutex_t *forks;
+
+} t_info;
+
 typedef struct philo
 {
-  int number_of_philo;
-  int time_to_die;
-  int time_to_eat;
-  int time_to_sleep;
-  int number_of_times_e_philo_must_eat;
-} philo;
+  int id;
+  pthread_mutex_t right_fork;
+  pthread_mutex_t *leftfork;
+  pthread_t     my_thread;
+  t_info  *info;
+
+} t_philo;
+
+
+
+
 #endif
